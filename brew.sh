70c66a83ae538f478fd0ae86f74774850a971f0e
Kazuaki NAKAMURA <n5@viiph.com>
2018-01-28 13:09:55 +0900

    Modified brew.sh
    add application

--- brew.sh
+++ brew.sh
@@ -2,25 +2,29 @@ brew update
 brew upgrade
 brew tap caskroom/versions
 
-N
+
 brew install zsh
 
-# brew cask install 1password
+brew cask install adobe-creative-cloud
 brew cask install alfred
 brew cask install appcleaner
 brew cask install atom
-# brew cask install bartender
+brew cask install brackets
+brew cask install busycal
+brew cask install busycontacts
+brew cask install cleanmymac
+brew cask install coda
+brew cask install codekit
 brew cask install contexts
-# brew cask install dropbox
-# brew cask install divvy
+brew cask install firefox
 brew cask install google-chrome
 brew cask install google-chrome-canary
-# brew cask install google-japanese-ime
+brew cask install hyperdock
 brew cask install imagealpha
 brew cask install imageoptim
 brew cask install iterm2
-# brew cask install popclip
-brew cask install sketch
+brew cask install resilio-sync
+brew cask install rightfont
 brew cask install skype
 brew cask install slack
 brew cask install spectacle
