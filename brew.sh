c64ab8d93e213f642b81cf5e11b990db6da6fee6
Kazuaki NAKAMURA <n5@viiph.com>
2020-12-28 11:10:42 +0900

    modified brew,sh

--- brew.sh
+++ brew.sh
@@ -13,6 +13,7 @@ brew cask install appcode
 brew cask install atom
 brew cask install bartender
 brew cask install bettertouchtool
+brew cask install biscuit
 brew cask install brackets
 brew cask install busycal
 brew cask install busycontacts
@@ -21,10 +22,10 @@ brew cask install clion
 brew cask install coda
 brew cask install codekit
 brew cask install contexts
+brew cask install dash
 brew cask install datagrip
 brew cask install default-folder-x
-brew cask install discord
-brew cask install dropbox
+# brew cask install dropbox
 brew cask install firefox
 brew cask install goland
 brew cask install google-chrome
@@ -36,22 +37,27 @@ brew cask install imagealpha
 brew cask install imageoptim
 brew cask install intellij-idea
 brew cask install iterm2
+brew cask install kaleidoscope
 brew cask install moom
+brew cask install mmhmm
 brew cask install notion
 brew cask install phpstorm
 brew cask install pycharm
 brew cask install resilio-sync
 brew cask install rider
 brew cask install rightfont
+brew cask install rubymine
 brew cask install slack
 brew cask install spectacle
+brew cask install spectrum
 brew cask install staruml
 brew cask install sublime-text
+brew cask install tableplus
 brew cask install telegram
 brew cask install tg-pro
 brew cask install tower
 brew cask install visual-studio-code
-brew cask install vmware-fusion
+# brew cask install vmware-fusion
 brew cask install webstorm
 brew cask install zeplin
 brew cask install zoomus
