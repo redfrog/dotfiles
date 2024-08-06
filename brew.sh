d73a13bee7e78d5b43a7b85ac9114fe13f2ae30f
Kazuaki NAKAMURA <n5@viiph.com>
2024-08-06 14:57:00 +0900

    modified brew.sh

--- brew.sh
+++ brew.sh
@@ -7,63 +7,78 @@ brew install tmux
 # zsh is default terminal in catalina
 brew install zsh
 
+# Homebrew Formulae
+# https://formulae.brew.sh
 
-# brew install --cask 1password
-brew install adobe-creative-cloud
-brew install alfred
-brew install appcleaner
-brew install appcode
-#brew install atom
-brew install bartender
-brew install bettertouchtool
-#brew install biscuit
-brew install brackets
-# brew install busycal
+brew install --cask 1password
+brew install --cask adobe-creative-cloud
+brew install --cask alfred
+brew install --cask arc
+brew install --cask appcleaner
+# brew install --cask appcode
+brew install --cask bartender
+brew install --cask bettertouchtool
+# brew install --cask biscuit
+# brew install --cask brackets
+# brew install --cask busycal
 # brew install --cask busycontacts
-brew install cleanmymac
-brew install clion
-brew install coda
+brew install --cask carbon-copy-cloner
+brew install --cask chatgpt
+brew install --cask chromium
+brew install --cask cleanmymac
+# brew install --cask clion
+# brew install --cask coda
 # brew install codekit
-brew install contexts
-brew install dash
-brew install datagrip
-brew install default-folder-x
+brew install --cask contexts
+brew install --cask cursor
+brew install --cask dash
+brew install --cask datagrip
+brew install --cask deepl
+brew install --cask default-folder-x
+# brew install docker
 # brew install dropbox
-brew install firefox
-brew install goland
-brew install google-chrome
-#brew install google-drive
-brew install grammarly
-brew install hyperdock
-brew install imagealpha
-brew install imageoptim
-brew install intellij-idea
-brew install iterm2
-brew install kaleidoscope
-# brew install kawa
+brew install --cask eset-cyber-security
+brew install --cask firefox
+brew install --cask firefox@developer-edition
+brew install --cask fleet
+# brew install --cask goland
+brew install --cask google-chrome
+brew install --cask google-chrome@canary
+# brew install --cask google-drive
+# brew install --cask grammarly
+brew install --cask hyperdock
+brew install --cask imagealpha
+brew install --cask imageoptim
+# brew install --cask intellij-idea
+brew install --cask iterm2
+brew install --cask jetbrains-toolbox
+brew install --cask kaleidoscope
+# brew install --cask kawa
 # brew install --cask moom
 # brew install --cask mmhmm
-brew install notion
-brew install phpstorm
-brew install pycharm
-brew install resilio-sync
-brew install rider
-brew install rightfont
-brew install rubymine
-brew install staruml
-brew install slack
-brew install spectacle
-brew install spectrum
-brew install staruml
-brew install sublime-text
-brew install tableplus
-brew install telegram
-brew install tg-pro
-brew install tower
+brew install --cask notion
+brew install --cask notion-calendar
+brew install --cask parallels
+# brew install --cask phpstorm
+# brew install --cask pycharm
+brew install --cask rectangle
+brew install --cask resilio-sync
+# brew install --cask rider
+brew install --cask rightfont
+# brew install --cask rubymine
+# brew install --cask slack
+# brew install spectacle
+brew install --cask spectrum
+brew install --cask staruml
+brew install --cask sublime-text
+brew install --cask tableplus
+brew install --cask telegram
+brew install --cask tg-pro
+brew install --cask tower
 brew install visual-studio-code
 # brew cask install --cask vmware-fusion
-brew install webstorm
-brew install zeplin
-brew install zoom
+# brew install --cask webstorm
+brew install --cask zeplin
+brew install --cask zoom
 
 brew cleanup
