3a71cf9c25de732a932b82d516d8ec476f4f7ceb
Kazuaki NAKAMURA <n5@viiph.com>
2024-08-12 20:25:55 +0900

    modified brew.sh

--- brew.sh
+++ brew.sh
@@ -7,33 +7,31 @@ brew install tmux
 # zsh is default terminal in catalina
 brew install zsh
 
+# diff-so-fancy
+brew install diff-so-fancy
+
 # Homebrew Formulae
 # https://formulae.brew.sh
 
-brew install --cask 1password
+# brew install --cask 1password
 # brew install --cask adobe-creative-cloud
 brew install --cask alfred
-brew install --cask arc
 brew install --cask appcleaner
-# brew install --cask appcode
+brew install --cask arc
 brew install --cask bartender
 brew install --cask bettertouchtool
-# brew install --cask biscuit
-# brew install --cask brackets
 # brew install --cask busycal
 # brew install --cask busycontacts
 brew install --cask carbon-copy-cloner
 brew install --cask chatgpt
 brew install --cask chromium
 brew install --cask cleanmymac
-# brew install --cask clion
 brew install --cask clipy
 # brew install --cask coda
 # brew install codekit
 brew install --cask contexts
 brew install --cask cursor
 brew install --cask dash
-brew install --cask datagrip
 brew install --cask deepl
 brew install --cask default-folder-x
 # brew install docker
@@ -41,8 +39,6 @@ brew install --cask default-folder-x
 brew install --cask eset-cyber-security
 brew install --cask firefox
 brew install --cask firefox@developer-edition
-brew install --cask fleet
-# brew install --cask goland
 brew install --cask google-chrome
 brew install --cask google-chrome@canary
 # brew install --cask google-drive
@@ -50,7 +46,6 @@ brew install --cask google-chrome@canary
 brew install --cask hyperdock
 brew install --cask imagealpha
 brew install --cask imageoptim
-# brew install --cask intellij-idea
 brew install --cask iterm2
 brew install --cask jetbrains-toolbox
 brew install --cask kaleidoscope
@@ -60,15 +55,10 @@ brew install --cask kaleidoscope
 brew install --cask notion
 brew install --cask notion-calendar
 brew install --cask parallels
-# brew install --cask phpstorm
-# brew install --cask pycharm
 brew install --cask rectangle
 brew install --cask resilio-sync
-# brew install --cask rider
 brew install --cask rightfont
-# brew install --cask rubymine
 # brew install --cask slack
-# brew install spectacle
 brew install --cask spectrum
 brew install --cask staruml
 brew install --cask sublime-text
